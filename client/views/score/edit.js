9f0ac74b42e5938a9ded5429da376599c1445bde
telminov <telminov@soft-way.biz>
2015-06-18 16:05:29 +0300

    edit and delete score

---
+++ client/views/score/edit.js
@@ -0,0 +1,32 @@
+Template.scoreEdit.helpers({
+    canEdit: function () { return canEditScore(Meteor.user(), this) }
+});
+
+Template.scoreEdit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+        var currentScore = this._id;
+
+        var scoreData = {
+            description: $(e.target).find('[name=description]').val(),
+            value: $(e.target).find('[name=value]').val(),
+            ownerUserName: $(e.target).find('[name=owner]').val()
+        };
+
+        Score.update(currentScore, {$set: scoreData}, function (error) {
+            if (error) {
+                alert(error.reason);
+            } else {
+                Router.go('scoreList');
+            }
+        })
+    },
+
+    'click .delete': function(e) {
+        if (confirm('Удалить?')) {
+            var currentScore = this._id;
+            Score.remove(currentScore);
+            Router.go('scoreList');
+        }
+    }
+});
--- client/views/score/list.js
+++ client/views/score/list.js
@@ -1,5 +1,5 @@
 Template.scoreList.helpers({
     score: function() {
         return Score.find({}, {sort: {date: -1}});
-    }
+    },
 });
